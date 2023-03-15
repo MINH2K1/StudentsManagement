a880816b676945455ece3b4a71b9de1db9eb58c2
tranducminh <minhk19caothang@gmail.com>
2023-03-15 18:50:35 +0700

    create_controller

---
+++ StudentManagement.Service/StudentResponsitory/StudentRepo.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.StudentDB;
+using StudentsManagement.Infastructure.IReponsitory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsManagement.Service.StudentResponsitory
+{
+    public class StudentRepo : IStudentRepo
+    {
+        private IReponsitory<Student> _reponsitory;
+
+        public StudentRepo(IReponsitory<Student> reponsitory)
+        {
+            _reponsitory = reponsitory;
+        }
+    
+        public IEnumerable<Student> GetAll()
+        {
+            return _reponsitory.GetAll();
+        }
+
+        public Student GetById(int id)
+        {
+           return _reponsitory.GetById(id);
+        }
+    }
+}
+
